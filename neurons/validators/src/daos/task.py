76816924371a6e8b19bec1c63017bdce72b92c16
pyon12 <pyon123@proton.me>
2024-09-10 12:56:00 -0400

    feat: update logic to calculate avg_score

--- neurons/validators/src/daos/task.py
+++ neurons/validators/src/daos/task.py
@@ -7,9 +7,11 @@ from models.task import Task, TaskStatus
 
 from daos.base import BaseDao
 
+
 class MinerAvgScore(BaseModel):
     miner_hotkey: str
     avg_score: float
+
 
 class TaskDao(BaseDao):
     def save(self, task: Task) -> Task:
@@ -30,18 +32,24 @@ class TaskDao(BaseDao):
         self.session.commit()
         self.session.refresh(task)
         return task
-    
+
     def get_avg_scores_in_hours(self, hours: int) -> List[MinerAvgScore]:
         hours_ago = datetime.now() - timedelta(hours=hours)
-        
-        results = self.session.query(
+
+        subquery = self.session.query(
             Task.miner_hotkey,
+            Task.executor_id,
             func.avg(Task.score).label('avg_score')
         ).filter(
             Task.task_status.in_([TaskStatus.Finished, TaskStatus.Failed]),
             Task.created_at >= hours_ago
-        ).group_by(Task.miner_hotkey).all()
+        ).group_by(Task.miner_hotkey, Task.executor_id).subquery()
         
+        results = self.session.query(
+            subquery.c.miner_hotkey,
+            func.sum(subquery.c.avg_score).label('score')
+        ).group_by(subquery.c.miner_hotkey).all()
+
         return [
             MinerAvgScore(
                 miner_hotkey=result[0],
@@ -49,7 +57,6 @@ class TaskDao(BaseDao):
             )
             for result in results
         ]
-        
-        
+
     def get_task_by_uuid(self, uuid: str) -> Task:
         return self.session.query(Task).filter_by(uuid=uuid).first()
