c0a804f106d0cfddd424d6888257f99fab05aa19
pyon12 <pyon123@proton.me>
2024-09-25 14:54:15 -0400

    feat: add max score of gpu series

---
+++ neurons/validators/src/services/const.py
@@ -0,0 +1,40 @@
+GPU_MAX_SCORES = {
+    # NVIDIA GeForce RTX Series
+    "NVIDIA GeForce RTX 3060 Ti": 0.35,
+    "NVIDIA GeForce RTX 3070": 0.40,
+    "NVIDIA GeForce RTX 3080": 0.50,
+    "NVIDIA GeForce RTX 3090": 0.70,
+    "NVIDIA GeForce RTX 4090": 1.00,
+
+    # NVIDIA Titan Series
+    "NVIDIA Titan RTX": 1.50,
+
+    # NVIDIA Quadro RTX Series
+    "NVIDIA Quadro RTX 4000": 1.00,
+    "NVIDIA Quadro RTX 5000": 1.50,
+    "NVIDIA Quadro RTX 6000": 2.20,
+    "NVIDIA Quadro RTX 8000": 2.50,
+    "NVIDIA Quadro RTX A4000": 1.20,
+    "NVIDIA Quadro RTX A5000": 1.80,
+    "NVIDIA Quadro RTX A6000": 3.00,
+
+    # NVIDIA A Series (Ampere Architecture)
+    "NVIDIA A100 Tensor Core (40 GB)": 3.00,
+    "NVIDIA A100 Tensor Core (80 GB)": 3.50,
+    "NVIDIA A4000": 0.80,
+    "NVIDIA RTX A4000": 0.80,
+    "NVIDIA A5000": 1.20,
+    "NVIDIA RTX A5000": 1.20,
+
+    # NVIDIA H Series (Hopper Architecture)
+    "NVIDIA H100 Tensor Core (80 GB)": 5.00,
+    "NVIDIA H200 Tensor Core (140 GB)": 7.00,
+
+    # NVIDIA T Series
+    "NVIDIA T4 Tensor Core": 0.30,
+
+    # Older NVIDIA Models
+    "NVIDIA GTX 1080 Ti": 0.25,
+    "NVIDIA RTX 6000 Ada": 2.00,
+    "NVIDIA RTX 8000 Ada": 2.50,
+}
