b7a4c89e70c6d1e63fc71eddccaed8c6804708ad
waris <waris0609@outlook.com>
2025-02-05 00:22:51 -0800

    chore: add comments and unused variables

--- neurons/validators/src/services/file_encrypt_service.py
+++ neurons/validators/src/services/file_encrypt_service.py
@@ -1,18 +1,18 @@
 import os
 import random
-import subprocess
-from typing import Annotated
-from pathlib import Path
-import tempfile
 import shutil
-import PyInstaller.__main__
-import sys
-from fastapi import Depends
 import string
+import subprocess
+import sys
+import tempfile
+from pathlib import Path
+from typing import Annotated
+
+import PyInstaller.__main__
+from fastapi import Depends
+from payload_models.payloads import MinerJobEnryptedFiles
 
 from services.ssh_service import SSHService
-
-from payload_models.payloads import MinerJobEnryptedFiles
 
 
 class FileEncryptService:
@@ -26,49 +26,66 @@ class FileEncryptService:
 
     def make_obfuscated_file(self, tmp_directory: str, file_path: str):
         subprocess.run(
-            ['pyarmor', 'gen', '-O', tmp_directory, file_path],
-            stdout=subprocess.PIPE,
-            stderr=subprocess.PIPE,
+            ["pyarmor", "gen", "-O", tmp_directory, file_path],
+            capture_output=True,
         )
         return os.path.basename(file_path)
 
     def make_binary_file(self, tmp_directory: str, file_path: str):
         file_name = os.path.basename(file_path)
 
-        PyInstaller.__main__.run([
-            file_path,
-            '--onefile',
-            '--noconsole',
-            '--log-level=ERROR',
-            '--distpath', tmp_directory,
-            '--name', file_name,
-        ])
+        PyInstaller.__main__.run(
+            [
+                file_path,
+                "--onefile",
+                "--noconsole",
+                "--log-level=ERROR",
+                "--distpath",
+                tmp_directory,
+                "--name",
+                file_name,
+            ]
+        )
 
-        subprocess.run(['rm', '-rf', 'build', f'{file_name}.spec'])
+        subprocess.run(["rm", "-rf", "build", f"{file_name}.spec"])
 
         return file_name
 
     def make_binary_file_with_nuitka(self, tmp_directory: str, file_path: str):
         file_name = os.path.basename(file_path)
 
-        subprocess.run([
-            'nuitka', '--standalone', '--onefile',
-            f'--output-dir={tmp_directory}',
-            '--remove-output', '--quiet', '--no-progress',
-            f'--output-filename={file_name}',
-            file_path
-        ])
+        subprocess.run(
+            [
+                "nuitka",
+                "--standalone",
+                "--onefile",
+                f"--output-dir={tmp_directory}",
+                "--remove-output",
+                "--quiet",
+                "--no-progress",
+                f"--output-filename={file_name}",
+                file_path,
+            ]
+        )
 
         return file_name
 
     def generate_random_name(self, length=10):
-        return '_' + ''.join(random.choices(string.ascii_letters, k=length))
-    
+        return "_" + "".join(random.choices(string.ascii_letters, k=length))
+
     def generate_key_mappings(self):
         keys_for_encryption_key_generation = [
-            'gpu.name', 'gpu.uuid', 'gpu.capacity', 'gpu.cuda', 'gpu.power_limit', 
-            'gpu.graphics_speed', 'gpu.memory_speed', 'gpu.pcie', 'gpu.speed_pcie', 'gpu.utilization', 
-            'gpu.memory_utilization'
+            "gpu.name",
+            "gpu.uuid",
+            "gpu.capacity",
+            "gpu.cuda",
+            "gpu.power_limit",
+            "gpu.graphics_speed",
+            "gpu.memory_speed",
+            "gpu.pcie",
+            "gpu.speed_pcie",
+            "gpu.utilization",
+            "gpu.memory_utilization",
         ]
 
         all_keys = {
@@ -82,9 +99,9 @@ class FileEncryptService:
             "gpu.pcie": "",
             "gpu.speed_pcie": "",
             "gpu.utilization": "",
-            "gpu.memory_utilization": ""
+            "gpu.memory_utilization": "",
         }
-        
+
         original_keys = {
             "gpu.name": "name",
             "gpu.uuid": "uuid",
@@ -96,7 +113,7 @@ class FileEncryptService:
             "gpu.pcie": "pcie",
             "gpu.speed_pcie": "pcie_speed",
             "gpu.utilization": "gpu_utilization",
-            "gpu.memory_utilization": "memory_utilization"
+            "gpu.memory_utilization": "memory_utilization",
         }
 
         # Generate dictionary key mapping on validator side
@@ -105,25 +122,35 @@ class FileEncryptService:
 
         self.all_keys = all_keys
         self.original_keys = original_keys
-        
+
         encryption_key = ":".join([all_keys[key] for key in keys_for_encryption_key_generation])
         return all_keys, encryption_key
 
     def get_original_key(self, key: str):
         return self.original_keys.get(key, key)
-    
+
     def get_all_keys(self):
         return self.all_keys
-    
+
     def ecrypt_miner_job_files(self):
+        """
+        Encrypts and obfuscates miner job files for secure execution.
+
+        This function performs the following steps:
+        1. Clears any existing temporary directory used for storing encrypted files.
+        2. Defines file paths for the machine scrape script and its obfuscator.
+        3. Runs the obfuscator script to generate an obfuscated version of the machine scrape script.
+        4. Replaces dictionary keys in the obfuscated script with randomly generated names.
+        5. Compiles the obfuscated script into a binary using Nuitka or another method.
+        6. Generates a score script file and obfuscates it for secure execution.
+
+        Returns: MinerJobEnryptedFiles
+        """
         tmp_directory = Path(__file__).parent / "temp"
         if tmp_directory.exists() and tmp_directory.is_dir():
             shutil.rmtree(tmp_directory)
 
-        # first chracter of variable shouldn't be digit
-        encrypt_key_name = self.ssh_service.generate_random_string(random.randint(10, 100), True)
-        encrypt_key_value = self.ssh_service.generate_random_string(random.randint(10, 100), False)
-
+        # file pathes
         machine_scrape_file_path = str(
             Path(__file__).parent / ".." / "miner_jobs/machine_scrape.py"
         )
@@ -133,37 +160,42 @@ class FileEncryptService:
         obfuscated_machine_scrape_file_path = str(
             Path(__file__).parent / ".." / "miner_jobs/obfuscated_machine_scrape.py"
         )
-        with open(machine_scrape_file_path, 'r') as file:
-            content = file.read()
 
+        # run obfuscator.py to generate obfuscated_machine_scrape.py
         command = [sys.executable, obfuscator_machine_scrape_file_path, machine_scrape_file_path]
-        subprocess.run(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-        
-        with open(obfuscated_machine_scrape_file_path, 'r') as file:
+        subprocess.run(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)  # noqa
+
+        with open(obfuscated_machine_scrape_file_path) as file:
             obfuscated_content = file.read()
-            
+
+        # replace dictionary keys with random names
         all_keys, encryption_key = self.generate_key_mappings()
         for key, value in all_keys.items():
             obfuscated_content = obfuscated_content.replace(key, value)
 
+        # build binary with nuitka
         with tempfile.NamedTemporaryFile(delete=True) as machine_scrape_file:
-            machine_scrape_file.write(obfuscated_content.encode('utf-8'))
+            machine_scrape_file.write(obfuscated_content.encode("utf-8"))
             machine_scrape_file.flush()
             os.fsync(machine_scrape_file.fileno())
 
             if random.choice([True, False]):
-                machine_scrape_file_name = self.make_binary_file_with_nuitka(str(tmp_directory), machine_scrape_file.name)
+                machine_scrape_file_name = self.make_binary_file_with_nuitka(
+                    str(tmp_directory), machine_scrape_file.name
+                )
             else:
-                machine_scrape_file_name = self.make_binary_file(str(tmp_directory), machine_scrape_file.name)
+                machine_scrape_file_name = self.make_binary_file(
+                    str(tmp_directory), machine_scrape_file.name
+                )
 
         # generate score_script file
         score_script_file_path = str(Path(__file__).parent / ".." / "miner_jobs/score.py")
-        with open(score_script_file_path, 'r') as file:
+        with open(score_script_file_path) as file:
             content = file.read()
         modified_content = content
 
-        with tempfile.NamedTemporaryFile(delete=True, suffix='.py') as score_file:
-            score_file.write(modified_content.encode('utf-8'))
+        with tempfile.NamedTemporaryFile(delete=True, suffix=".py") as score_file:
+            score_file.write(modified_content.encode("utf-8"))
             score_file.flush()
             os.fsync(score_file.fileno())
             score_file_name = self.make_obfuscated_file(str(tmp_directory), score_file.name)
