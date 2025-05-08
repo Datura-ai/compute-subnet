37550c3fa4926118e6a9da33e7b2bdbc12d4217a
pyon12 <pyon123@proton.me>
2025-05-08 16:42:19 +0000

    update log query

--- neurons/executor/src/daos/pod_log.py
+++ neurons/executor/src/daos/pod_log.py
@@ -7,6 +7,6 @@ from models.pod_log import PodLog
 class PodLogDao(BaseDao[PodLog]):
     def find_by_continer_name(self, session: Session, container_name: str) -> list[PodLog]:
         statement = select(PodLog).where(
-            PodLog.container_name == container_name or PodLog.container_name is None
+            (PodLog.container_name == container_name) | (PodLog.container_name == None)
         ).order_by(PodLog.created_at)
         return session.exec(statement).all()
