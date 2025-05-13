a20b47ca5cdd53432c60d3a90ffd3f0e2c24e256
pyon12 <pyon123@proton.me>
2025-05-13 17:44:47 +0000

    skip container check when it's rented

--- neurons/validators/src/services/redis_service.py
+++ neurons/validators/src/services/redis_service.py
@@ -134,11 +134,7 @@ class RedisService:
         if not data:
             return None
 
-        data = json.loads(data)
-        if not data.get("container_name", ""):
-            return None
-
-        return data
+        return json.loads(data)
 
     async def add_executor_uptime(self, machine: ExecutorUptimeResponse):
         await self.hset(EXECUTORS_UPTIME_PREFIX, f"{machine.executor_ip_address}:{machine.executor_ip_port}", str(machine.uptime_in_minutes))
