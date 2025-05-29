14207282a6a50b200643fa9297cde6a33bfb57bc
SuperDev <wasi949916@hotmail.com>
2025-05-29 11:19:18 -0700

    hotfix: DAH-1286 - fix UUID to str

--- neurons/miners/src/cli.py
+++ neurons/miners/src/cli.py
@@ -165,8 +165,8 @@ def remove_executor(address: str, port: int, reclaim_amount:float, reclaim_descr
 
                 eligible_executors = await collateral_contract.get_eligible_executors()
                 logger.info(f"All eligible executors: {eligible_executors}")
-                logger.info(f"Current executor is eligible: {executor_uuid in eligible_executors}")
-                if not executor_uuid in eligible_executors:
+                logger.info(f"Current executor is eligible: {str(executor_uuid) in eligible_executors}")
+                if not str(executor_uuid) in eligible_executors:
                     executor_dao.delete_by_address_port(address, port)
                     logger.info("Removed executor (%s:%d)", address, port)
                 else:
