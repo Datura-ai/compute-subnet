02d0bc84c2ba0c325485da2528ac5653331d7661
waris <waris0609@outlook.com>
2025-06-18 08:23:42 -0700

    feat(validator): DAH-1286 - update logic for required deposit amount calculation

--- neurons/validators/src/services/collateral_contract_service.py
+++ neurons/validators/src/services/collateral_contract_service.py
@@ -7,28 +7,9 @@ from typing import Optional, List, Dict, Any
 from datura.requests.miner_requests import ExecutorSSHInfo
 from core.utils import _m, get_extra_info, get_collateral_contract
 from core.config import settings
+from services.const import REQUIRED_DEPOSIT_AMOUNT
 
 logger = logging.getLogger(__name__)
-
-
-async def get_available_machines() -> Optional[List[Dict[str, Any]]]:
-    """Get available machines from the compute REST API."""
-    url = f"{settings.COMPUTE_REST_API_URL}/machines"
-    try:
-        async with aiohttp.ClientSession() as session:
-            async with session.get(url, timeout=aiohttp.ClientTimeout(total=60)) as response:
-                if response.status != 200:
-                    error_msg = await response.text()
-                    logger.error(f"Error {response.status}: Unable to retrieve available machines. Details: {error_msg}")
-                    return None
-                try:
-                    return await response.json()
-                except aiohttp.ContentTypeError:
-                    logger.error("Invalid JSON response from machine API.")
-                    return None
-    except Exception as e:
-        logger.error(f"Exception while fetching available machines: {e}", exc_info=True)
-        return None
 
 
 class CollateralContractService:
@@ -89,40 +70,8 @@ class CollateralContractService:
 
 
     async def _get_gpu_required_deposit(self, gpu_model: str, gpu_count:int, extra: Dict[str, Any]) -> Optional[float]:
-        machines = await get_available_machines()
-        if machines is None:
-            logger.error("Could not fetch available machines for GPU pricing.")
-            return None
-
-        gpu_entry = next((g for g in machines if g.get("name") == gpu_model), None)
-        if not gpu_entry:
-            logger.error(f"GPU '{gpu_model}' not found in available machines.")
-            return None
-
-        gpu_base_price = gpu_entry.get("base_price")
-        rate_float = self.get_tao_price_in_usd()
-        required_deposit_amount = float(gpu_base_price) / rate_float * settings.COLLATERAL_HOURS * gpu_count
-
-        self._log_error(
-            "GPU required deposit amount calculated",
-            extra,
-            gpu_model=gpu_model,
-            required_deposit_amount=required_deposit_amount,
-            gpu_base_price=gpu_base_price,
-            rate_float=rate_float
-        )
-        
-        if required_deposit_amount is None or not isinstance(required_deposit_amount, (int, float)):
-            self._log_error(
-                "Deposit amount for GPU is invalid or missing",
-                extra,
-                gpu_model=gpu_model,
-                required_deposit_amount=required_deposit_amount,
-                gpu_base_price=gpu_base_price,
-                rate_float=rate_float
-            )
-            return None
-
+        unit_tao_amount = REQUIRED_DEPOSIT_AMOUNT[gpu_model]
+        required_deposit_amount = unit_tao_amount * gpu_count * settings.COLLATERAL_DAYS
         return float(required_deposit_amount)
 
     def _log_error(self, message: str, extra: Dict[str, Any], exc_info: bool = False, **kwargs):
