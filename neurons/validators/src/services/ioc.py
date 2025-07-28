6aae1be021183fa0521691a9a50ecff756a3328e
SuperDev <wasi949916@hotmail.com>
2025-07-28 11:34:26 -0700

    chore: DAH-1383 - remove collateral_contract_service at minerservice of ioc

--- neurons/validators/src/services/ioc.py
+++ neurons/validators/src/services/ioc.py
@@ -33,8 +33,7 @@ async def initiate_services():
     ioc["MinerService"] = MinerService(
         ssh_service=ioc["SSHService"],
         task_service=ioc["TaskService"],
-        redis_service=ioc["RedisService"],
-        collateral_contract_service=ioc["CollateralContractService"]
+        redis_service=ioc["RedisService"]
     )
 
 
